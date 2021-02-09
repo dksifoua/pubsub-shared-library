9764468e65758c7e4af5bed8d389d5c7b5d1250a
Dimitri Kemgang Sifoua <dimitri.kemgangsifoua@nbc.ca>
2021-02-09 10:14:14 -0500

    Fix publish script calling

--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,4 +1,4 @@
 def call(String projectId, String topicId, String message) {
-    sh """python -m forest.pubsub.publishPubsubMessage --project_id=${projectId} \
+    sh """python -m forest.pubsub.publish --project_id=${projectId} \
             --topic_id=${topicId} --message=${message}"""
 }
