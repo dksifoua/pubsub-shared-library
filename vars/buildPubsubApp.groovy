856b027422f47a3daca90d2672d601e66eabeadb
Dimitri Kemgang Sifoua <dimitri.kemgangsifoua@nbc.ca>
2021-02-09 10:08:31 -0500

    Remove stages

--- vars/buildPubsubApp.groovy
+++ vars/buildPubsubApp.groovy
@@ -1,11 +1,7 @@
 def call() {
-    stage('Clone') {
-        git branch: "feature/IAI-3045",
-                url: "https://git.bnc.ca/scm/app9202/forest.git"
-    }
+    git branch: "feature/IAI-3045",
+            url: "https://git.bnc.ca/scm/app9202/forest.git"
 
-    stage('Build') {
-        sh """python -m pip install google-cloud-pubsub --trusted-host pypi.python.org \
+    sh """python -m pip install google-cloud-pubsub --trusted-host pypi.python.org \
                     --trusted-host files.pythonhosted.org --trusted-host pypi.org"""
-    }
 }
