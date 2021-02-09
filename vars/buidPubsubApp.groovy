6590f8f7f5e5ce81e60c9d590b7f7d12e897416c
Dimitri Kemgang Sifoua <dimitri.kemgangsifoua@nbc.ca>
2021-02-09 09:44:54 -0500

    fix

--- vars/buidPubsubApp.groovy
+++ vars/buidPubsubApp.groovy
@@ -1,17 +1,15 @@
 def call() {
-    node {
-        stage('Clone') {
-            steps {
-                git branch: "feature/IAI-3045",
-                        url: "https://git.bnc.ca/scm/app9202/forest.git"
-            }
+    stage('Clone') {
+        steps {
+            git branch: "feature/IAI-3045",
+                    url: "https://git.bnc.ca/scm/app9202/forest.git"
         }
+    }
 
-        stage('Build') {
-            steps {
-                sh """python -m pip install google-cloud-pubsub --trusted-host pypi.python.org \
+    stage('Build') {
+        steps {
+            sh """python -m pip install google-cloud-pubsub --trusted-host pypi.python.org \
                     --trusted-host files.pythonhosted.org --trusted-host pypi.org"""
-            }
         }
     }
 }
