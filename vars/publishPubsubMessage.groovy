99d9f00bb56c96274a197aeea674a1f2a0b9f2ba
Dimitri K. Sifoua <dimitri.sifoua@gmail.com>
2021-02-10 13:27:14 -0500

    Update publishPubsubMessage.groovy

--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,3 +1,9 @@
 def call(String nexusUser, String nexusPassword, String projectId, String topicId, String message) {
-    echo "${nexusUser} ${nexusPassword} ${projectId} ${topicId} ${message}"
+    container.docker()
+            .setImage('9202/python-ci-agent:1.0.0-production')
+            .setRegistry('https://docker-production.bnc.ca')
+            .setRegistryCredentials(context.getToolId('artifact-management'))
+            .exec {
+                echo "${nexusUser} ${nexusPassword} ${projectId} ${topicId} ${message}"
+            }
 }
