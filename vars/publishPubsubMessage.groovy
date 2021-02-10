af54c440e6373512189ce608798991ad3933c1fc
Dimitri Kemgang Sifoua <dimitri.kemgangsifoua@nbc.ca>
2021-02-10 13:13:20 -0500

    Remove imports in Jenkins vars

--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,5 +1,3 @@
-import com.nbc.ci.PublishPubsubMessage
-
 def call(String nexusUser, String nexusPassword, String projectId, String topicId, String message) {
     container.docker()
             .setImage('9202/python-ci-agent:1.0.0-production')
