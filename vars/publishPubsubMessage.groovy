90dcb4d06fa1c1d12dbae927d287d10731deb34f
Dimitri K. Sifoua <dimitri.sifoua@gmail.com>
2021-02-10 13:33:06 -0500

    Update publishPubsubMessage.groovy

--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,6 +1,12 @@
 def call(String nexusUser, String nexusPassword, String projectId, String topicId, String message) {
-    def credentials = "${nexusUser}:${nexusPassword}"
-    def extraIndexUrl = "https://${credentials}@nexus.bnc.ca/repository/9202-pypi-production-local/simple"
-    sh "python -m pip install --extra-index-url ${extraIndexUrl} forest"
-    sh "python -m forest.pubsub.publish --project_id=${projectId} --topic_id=${topicId} --message=${message}"
+    container.docker()
+        .setImage('9202/python-ci-agent:1.0.0-production')
+        .setRegistry('https://docker-production.bnc.ca')
+        .setRegistryCredentials(context.getToolId('artifact-management'))
+        .exec {
+            def credentials = "${nexusUser}:${nexusPassword}"
+            def extraIndexUrl = "https://${credentials}@nexus.bnc.ca/repository/9202-pypi-production-local/simple"
+            sh "python -m pip install --extra-index-url ${extraIndexUrl} forest"
+            sh "python -m forest.pubsub.publish --project_id=${projectId} --topic_id=${topicId} --message=${message}"
+        }
 }
