86c91be8b4d6a7879a966c6328353b1cbe52f8c6
Dimitri K. Sifoua <dimitri.sifoua@gmail.com>
2021-02-10 14:12:55 -0500

    Update publishPubsubMessage.groovy

--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,5 +1,5 @@
 import com.nbc.ci.PublishPubsubMessage
 
 def call(String nexusUser, String nexusPassword) {
-    return new PublishPubsubMessage(this, nexusUser, nexusPassword)
+    return new PublishPubsubMessage(this, nexusUser, nexusPassword).publish("pot-ai-sandbox-8626", "test", "test")
 }
