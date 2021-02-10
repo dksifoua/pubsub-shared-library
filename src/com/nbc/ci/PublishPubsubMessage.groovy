7e4e240dc8da13ce5018b8353f64e130cebed4c9
Dimitri Kemgang Sifoua <dimitri.kemgangsifoua@nbc.ca>
2021-02-10 14:01:57 -0500

    clean code without docker container

---
+++ src/com/nbc/ci/PublishPubsubMessage.groovy
@@ -0,0 +1,22 @@
+package com.nbc.ci
+
+class PublishPubsubMessage {
+    protected Object steps
+    private String nexusUser
+    private String nexusPassword
+
+    PublishPubsubMessage(Object steps, String nexusUser, String nexusPassword) {
+        this.steps = steps
+        this.nexusUser = nexusUser
+        this.nexusPassword = nexusPassword
+
+        def credentials = "${this.nexusUser}:${this.nexusPassword}"
+        def extraIndexUrl = "https://${credentials}@nexus.bnc.ca/repository/9202-pypi-production-local/simple"
+        this.steps.sh("pip install --extra-index-url ${extraIndexUrl} forest")
+    }
+
+    def publish(String projectId, String topicId, String message) {
+        this.steps.sh("python -m forest.pubsub.publish --project_id=${projectId} --topic_id=${topicId} " +
+                    "--message=${message}")
+    }
+}
--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,6 +1,5 @@
+import com.nbc.ci.PublishPubsubMessage
+
 def call(String nexusUser, String nexusPassword, String projectId, String topicId, String message) {
-    def credentials = "${nexusUser}:${nexusPassword}"
-    def extraIndexUrl = "https://${credentials}@nexus.bnc.ca/repository/9202-pypi-production-local/simple"
-    sh "python -m pip install --extra-index-url ${extraIndexUrl} forest"
-    sh "python -m forest.pubsub.publish --project_id=${projectId} --topic_id=${topicId} --message=${message}"
+    return new PublishPubsubMessage(this, nexusUser, nexusPassword)
 }
