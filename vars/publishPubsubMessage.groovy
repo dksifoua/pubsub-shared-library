c6e56f61280325add8017775cdecb615187b024d
Dimitri K. Sifoua <dimitri.sifoua@gmail.com>
2021-02-10 13:18:22 -0500

    Update publishPubsubMessage.groovy

--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,6 +1,4 @@
 def call(String nexusUser, String nexusPassword, String projectId, String topicId, String message) {
-    sh script: "pip install --extra-index-url ${extraIndexUrl} forest", returnStdout: true
-    sh script: "python -m forest.pubsub.publish --project_id=${projectId} --topic_id=${topicId} " +
-                "--message=${message}",
-        returnStdout: true
+    sh "pip install --extra-index-url ${extraIndexUrl} forest"
+    sh "python -m forest.pubsub.publish --project_id=${projectId} --topic_id=${topicId} --message=${message}"
 }
