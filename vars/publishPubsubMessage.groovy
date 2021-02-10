9174346eae0b62c777348758c02bdb0c241d211b
Dimitri K. Sifoua <dimitri.sifoua@gmail.com>
2021-02-10 13:20:11 -0500

    Update publishPubsubMessage.groovy

--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,4 +1,3 @@
 def call(String nexusUser, String nexusPassword, String projectId, String topicId, String message) {
-    sh "pip install --extra-index-url ${extraIndexUrl} forest"
-    sh "python -m forest.pubsub.publish --project_id=${projectId} --topic_id=${topicId} --message=${message}"
+    echo "${nexusUser} ${nexusPassword} ${projectId} ${topicId} ${message}"
 }
