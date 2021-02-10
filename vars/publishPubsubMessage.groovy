29ead3fecca37f0f90c54ed079f57f2ea78bb204
Dimitri Kemgang Sifoua <dimitri.kemgangsifoua@nbc.ca>
2021-02-10 13:12:48 -0500

    Remove src folder

--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,9 +1,16 @@
 import com.nbc.ci.PublishPubsubMessage
 
 def call(String nexusUser, String nexusPassword, String projectId, String topicId, String message) {
-    docker = container.docker()
+    container.docker()
             .setImage('9202/python-ci-agent:1.0.0-production')
             .setRegistry('https://docker-production.bnc.ca')
-            .setRegistryCredentials(context.getToolId('artifact-management'));
-    return new PublishPubsubMessage(this, docker, nexusUser, nexusPassword, projectId, topicId, message)
+            .setRegistryCredentials(context.getToolId('artifact-management'))
+            .exec {
+                def credentials = "${nexusUser}:${nexusPassword}"
+                def extraIndexUrl = "https://${credentials}@nexus.bnc.ca/repository/9202-pypi-production-local/simple"
+                sh script: "pip install --extra-index-url ${extraIndexUrl} forest", returnStdout: true
+                sh script: "python -m forest.pubsub.publish --project_id=${projectId} --topic_id=${topicId} " +
+                            "--message=${message}",
+                    returnStdout: true
+            }
 }
