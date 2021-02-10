ee43654bc1fac43952e469432b4dd9ffeb1aa983
Dimitri K. Sifoua <dimitri.sifoua@gmail.com>
2021-02-10 13:31:28 -0500

    Update publishPubsubMessage.groovy

--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,6 +1,6 @@
 def call(String nexusUser, String nexusPassword, String projectId, String topicId, String message) {
     def credentials = "${nexusUser}:${nexusPassword}"
     def extraIndexUrl = "https://${credentials}@nexus.bnc.ca/repository/9202-pypi-production-local/simple"
-    sh "pip install --extra-index-url ${extraIndexUrl} forest"
+    sh "python -m pip install --extra-index-url ${extraIndexUrl} forest"
     sh "python -m forest.pubsub.publish --project_id=${projectId} --topic_id=${topicId} --message=${message}"
 }
