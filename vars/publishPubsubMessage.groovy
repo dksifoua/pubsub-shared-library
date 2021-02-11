859c19b1f7d661971d5516d7769d0efa2361c3f5
Dimitri K. Sifoua <dimitri.sifoua@gmail.com>
2021-02-11 09:35:23 -0500

    Update publishPubsubMessage.groovy

--- vars/publishPubsubMessage.groovy
+++ vars/publishPubsubMessage.groovy
@@ -1,5 +1,7 @@
 import com.nbc.ci.PublishPubsubMessage
 
 def call(String nexusUser, String nexusPassword) {
+    def forestInstalled = sh(script: "python -c \"import forest\"", returnStdout: true).trim()
+    echo "Is Forest installed? ${forestInstalled}"
     return new PublishPubsubMessage(this, nexusUser, nexusPassword)
 }
